1ae594c18b9a6dd02232fe9d5d56b406862e98dd
nazhiifyahya <mnazhiifys@gmail.com>
2025-04-26 16:44:35 +0700

    customizing map size

--- app/components/DonorRequestsMap.tsx
+++ app/components/DonorRequestsMap.tsx
@@ -8,28 +8,26 @@ interface MapProps {
   center: [number, number];
   zoom?: number;
   markerPosition?: [number, number];
-  markerPopup?: string;
+  
 }
 
 const MapComponent: React.FC<MapProps> = ({
   center,
   zoom = 13,
   markerPosition,
-  markerPopup,
 }) => {
   return (
     <MapContainer 
         center={center}
         zoom={zoom}
-        style={{ height: '400px', width: '100%' }
-    }>
+        style={{ height: '300px', width: '600px' }
+        }>
         <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {markerPosition && (
-        <Marker position={markerPosition}>
-        {markerPopup && <Popup>{markerPopup}</Popup>}
-        </Marker>
+          <Marker position={markerPosition}>
+          </Marker>
         )}
     </MapContainer>
   );
